e934730e23c676d778a0af4b59b38f6b284c5760
Gabriel-de-AvelarR <gabrieldeavelar@usp.br>
2025-06-13 15:59:10 -0300

    Comunicacao MQTT e maquina de estados do Ar

--- chatbot/core.py
+++ chatbot/core.py
@@ -1,5 +1,6 @@
 import spacy
 from pathlib import Path
+from mqtt_client import send_command
 
 class Chatbot:
     def __init__(self, model_path = None):
@@ -17,6 +18,8 @@ class Chatbot:
                 "consultar a temperatura": ["ROOM"],
                 "ajustar a temperatura": ["ROOM", "VALUE"]
             }
+
+            self.ac_states = {} #Exemplo:{"1": {"power": False, "temperature": 22}}
             
         except Exception as e:
             raise RuntimeError(f"Erro ao carregar o modelo: {e}")
@@ -73,11 +76,80 @@ class Chatbot:
                 "next_step": "ask_first_missing"
             }
         
-        return{
-            "status": "sucess",
-            "response": self.generate_response(intent, entities),
-            "entities": entities
-        }
+        #VERIFICACOES LOGICAS 
+
+        room = entities.get("ROOM")
+        if intent in ["desligar", "diminuir a  temperatura", "aumentar a temperatura", "ajustar a temperatura", "consultar a temperatura"]:
+            if room not in self.ac_states or self.ac_states[room].get("power", False):
+                return{
+                    "status": "error",
+                    "message": f"O ar condicionado da sala {room} esta desligado."
+                }
+            
+            elif intent in ["diminuir a  temperatura", "aumentar a temperatura"]:
+                current_temp = self.ac_states.get(room, {}).get("temperature")
+                try:
+                    value = int(entities.get("VALUE", 0))
+                    if intent == "diminuir a temperatura":
+                        entities["VALUE"] = str(current_temp - value)
+                    else:
+                        entities["VALUE"] = str(current_temp + value)
+                
+                except ValueError:
+                    return{
+                        "status": "error",
+                        "message": "Valor de temperatura invalido"
+                    }
+        
+        if intent == "ligar":
+            if self.ac_states[room].get("power", True):
+                return{
+                    "status": "error",
+                    "message": f"O ar condicionado da sala {room} já esta ligado."
+
+                }
+            else:
+                entities["VALUE"] = 20 #valor padrao para ligar o ar
+        
+        if intent == "no_intent":
+            return{
+                "status": "error",
+                "message": "Comando não listado, em que posso te ajudar?"
+                }
+
+
+        #COMUNICACAO MQTT
+        sucess = True #send_command(intent, entities)
+
+        if not sucess:
+            return{
+                "status": "error",
+                "message": "Falha na comunicação com o dispositivo IoT"
+            }
+
+        #ATUALIZACAO DA MAQUINA DE ESTADO DO AR
+        else:
+            if room:
+                if room not in self.ac_states:
+                    self.ac_states[room] = {}
+                
+                if intent == "ligar":
+                    self.ac_states[room]["power"] = True
+                    self.ac_states[room]["temperature"] = entities["VALUE"]
+                elif intent == "desligar":
+                    self.ac_states[room]["power"] = False
+                elif intent in ["diminuir a  temperatura", "aumentar a temperatura", "ajustar a temperatura"]:
+                    try:
+                        self.ac_states[room]["temperature"] = int(entities["VALUE"])
+                    except (ValueError, KeyError):
+                        pass
+
+
+            return{
+                "status": "sucess",
+                "response": self.generate_response(intent, entities),
+                "entities": entities
+            }
 
     '''Funcoes auxiliares'''
     
@@ -115,9 +187,8 @@ class Chatbot:
             "desligar": f"Ok, desligando o ar condicionado da sala {room}.",
             "diminuir a temperatura": f"Ok, diminuindo a temperatura da sala {room} em {value} graus.",
             "aumentar a temperatura": f"Ok, aumentando a temperatura da sala {room} em {value} graus.",
-            "consultar a temperatura": f"Ok, consultarei a temperatura atual da sala {room}.",
-            "ajustar a temperatura": f"Ok, ajustarei a temperatura da sala {room} em {value} graus.",
-            "no_intent": "Comando não listado, em que posso te ajudar?"
+            "consultar a temperatura": f"A temperatura atual da sala {room} é {self.ac_states[room]['temperature']} graus.",
+            "ajustar a temperatura": f"Ok, ajustarei a temperatura da sala {room} em {value} graus."
         }
         
         return responses.get(intent)
---
+++ chatbot/mqtt_client.py
@@ -0,0 +1,65 @@
+import paho.mqtt.client as mqtt
+import json 
+import time 
+
+def send_command(intent, entities):
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Conectado ao broker com sucesso")
+        else:
+            print(f"Falha na conexao. Codigo de retorno: {rc}")
+    
+    def on_disconnect(client, userdata, rc):
+        print("Desconectado do broker MQTT")
+        
+        if rc != 0:
+            print("Reconectando automaticamente")
+            try:
+                client.reconnect()
+            except Exception as e:
+                print(f"Erro ao tentar reconectar {e}")
+
+    def on_publish(client, userdata, mid):
+        print("Mensagem publicada com sucesso")
+
+    
+    client = mqtt.Client() 
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_publish = on_publish
+
+    try:
+        client.connect("andromeda.lasdpc.icmc.usp.br", 60102, 60)
+        client.loop_start()
+
+        room = entities.get("ROOM")
+        if room:
+            room = room.replace("sala", "").strip()
+        
+        intents_ligar = ["ligar", "ajustar a temperatura", "diminuir a temperatura", "aumentar a temperatura"]
+        intents_desligar = ["desligar"]
+
+        power = True if intent in intents_ligar else False if intent in intents_desligar else False
+        
+        payload = {
+            "enviromentId": room,
+            "deviceId": None,
+            "mode": 0,
+            "temperature": entities.get("VALUE", 20),
+            "power": power
+        }
+
+        client.publish("aircon/command/", json.dumps(payload))
+
+        client.loop(timeout = 2.0)
+        client.loop_stop()
+        client.disconnect()
+
+        return True
+
+    except Exception as e:
+        print(f"Erro ao enviar comando MQTT: {e}")
+        return False
+
+
+send_command("desligar", {"ROOM": "1"})
